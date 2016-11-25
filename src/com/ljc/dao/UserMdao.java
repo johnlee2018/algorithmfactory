0d68f65fd5c54886d4f311191e586fb4a5af22ac
lijianchun <1069815305@qq.com>
2016-11-25 22:14:03 +0800

    i create a UserMdao interface

---
+++ src/com/ljc/dao/UserMdao.java
@@ -0,0 +1,40 @@
+package com.ljc.dao;
+
+
+import java.util.List;
+import com.ljc.pojo.User;
+import com.ljc.pojo.PagePojo;
+
+/**
+ * @author HP
+ *
+ */
+public interface UserMdao{
+	
+	/**
+	 * 根据用户名查询单条记录
+	 * 
+	 * @author li jianchun
+	 * @param username
+	 * @return
+	 */
+	public User getUserByUserName(String username);
+	/**
+	 * 根据用户名和密码查询单条记录
+	 * 
+	 * @author li jianchun
+	 * @param user
+	 * @return
+	 */
+	public User getUserByUserNamePass(User user);
+
+	/**
+	 * 分页得到所有用户记录
+	 * @author li jianchun
+	 * @param pagePojo
+	 * @return
+	 */
+	public List <User> getUserByPage(PagePojo pagePojo);
+	
+	
+}
